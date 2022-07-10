a2968e6b3ccacc22e02df42e9dbf4615a8872638
emmageo3 <kanfanyemma22@gmail.com>
2022-07-10 17:38:22 +0000

    i am listening music

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\User;
@@ -33,6 +34,23 @@ class OrdersController extends Controller
             $data = $request->all();
             Order::where('id',$data['order_id'])->update(['order_status'=>$data['order_status']]);
             Session::put('success_message','Le statut de la commande a été mis à jour avec succès!');
+
+            $deliveryDetails = Order::select('email','name')->where('id', $data['order_id'])->first()->toArray();
+            $orderDetails = Order::with('orders_products')->where('id', $data['order_id'])->first()->toArray();
+
+                $email = $deliveryDetails['email'];
+                $messageData = [
+                    'email'=> $email,
+                    'name' => $deliveryDetails['name'],
+                    'order_id' => $data['order_id'],
+                    'order_status' => $data['order_status'],
+                    'orderDetails' =>  $orderDetails
+                ];
+
+                Mail::send('emails.order_status', $messageData, function($message) use($email){
+                    $message->to($email)->subject('Statut de votre commande - Evisem');
+                });
+
             return redirect()->back();
         }
     }
